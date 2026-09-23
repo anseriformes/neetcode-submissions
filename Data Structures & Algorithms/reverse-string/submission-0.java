2d4dc5f8afc8199c124139a2e016e7623426578f
anseriformes <anseriformes@users.noreply.github.com>
2026-09-22 21:01:41 -0700

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        char tmp;
+
+        while (i < j) {
+            tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+
+            i++;
+            j--;
+        }
+    }
+}
