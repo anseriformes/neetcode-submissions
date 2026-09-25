151315d637a8750daa3ae66c63ccbeadc5ce968f
anseriformes <anseriformes@users.noreply.github.com>
2026-09-25 14:52:07 -0700

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        boolean result = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; k >= j-i && j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    result = true;
+                    break;
+                }
+            }
+
+            if (result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
